7d31007e5c748ff6b91f46fe057f272a2c259a6a
Vladimir Yussupov <v.yussupov@gmail.com>
2020-05-26 18:50:17 +0200

    Update generated data
    Signed-off-by: Vladimir Yussupov <v.yussupov@gmail.com>

--- src/message_generator.py
+++ src/message_generator.py
@@ -3,6 +3,7 @@ import uuid
 import socket
 from logging import info
 from random import randint as randint
+from random import choice as choice
 from json import load
 from config import PROJECT_PATH
 
@@ -33,6 +34,7 @@ class MessageGenerator:
         * data: dict with timestamp
         :return: dict, representing the CloudEvent message
         """
+        service_type = ['standard', 'premium']
         msg = {
                 'id': uuid.uuid4().hex,
                 'time': datetime.now(timezone.utc).isoformat(),
@@ -40,5 +42,6 @@ class MessageGenerator:
                 'data': {
                     'timestamp': datetime.now().timestamp(),
                     'customerRating': randint(7, 10),
+                    'serviceType': choice(service_type)
                 }}
         return {**msg, **self.default_fields}
